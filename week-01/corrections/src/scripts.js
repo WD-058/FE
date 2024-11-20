4b9dcd04ab4657a8bbfd8b8466312354558f381d
mariblan <mariablancocanal@gmail.com>
2024-11-20 13:28:18 +0100

    Added lectures basics and arrays

---
+++ week-01/corrections/src/scripts.js
@@ -0,0 +1,27 @@
+const temperature = "18";
+
+// && ->  and
+// || =>  or
+// ! => not  - !=   !==
+// == ===
+
+if("18" != 18) {
+    console.log("Not 18")
+}
+
+if("18" !== 18) {
+    console.log("Not 18")
+}
+
+if (temperature < 15) {
+  console.log("It's cold, wear a coat");
+} else if (temperature <= 25) {
+  console.log('No nead to wear a coat.');
+} else {
+  console.log("It's warm, just wear a T-shirt");
+}
+
+
+
+
+
